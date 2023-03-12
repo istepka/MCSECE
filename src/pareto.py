caf9f2909c45ef3a4bcd23977bc83dcfffcd6675
Ignacy Stępka <ignacy.stepka@gmail.com>
2023-03-12 17:58:04 +0100

    Add ideal point method to pareto script

--- src/pareto.py
+++ src/pareto.py
@@ -26,5 +26,27 @@ def get_pareto_optimal_mask(data: npt.NDArray, optimization_direction: List[str]
 
     return mask
 
+def get_ideal_point(data: npt.NDArray, optimization_direction: List[str], pareto_optimal_mask: npt.NDArray) -> npt.NDArray:
+    '''
+    `data`: scores to explore   
+    `optimization direction`: list of ['max' and 'min'] of the data feature length
+    `pareto_optimal_mask`: mask to the data with 1 in positions of pareto optimal datapoints 
+
+    return ideal point of the pareto optimal points
+    '''
+    pareto_optimal_data = data[pareto_optimal_mask.astype(bool)]
+    # ideal point is the point with the best score for each objective
+    # it works in criteria space
+    ideal_point = np.zeros(data.shape[1])
+    
+    for i, opt in enumerate(optimization_direction):
+        if opt == 'max':
+            ideal_point[i] = np.max(pareto_optimal_data[:, i])
+        else:
+            ideal_point[i] = np.min(pareto_optimal_data[:, i])
+            
+    return ideal_point
+
 if __name__ == '__main__':
     get_pareto_optimal_mask(np.array([[0,1,2], [0,0,0], [1,1,1]]), ['max', 'max', 'max'])
+    print(get_ideal_point(np.array([[0,1,2], [0,0,0], [1,1,1]]), ['max', 'max', 'max'], np.array([1,0,1])))
